f15c23e951dfa1c378e406573189ccc2695c69eb
i5xia0 <i5xia0@icloud.com>
2021-04-21 16:52:18 +0800

    Revert "qihou01"
    This reverts commit d52933d32c2ff98405e23ef3e6b9f16dcfe9b4d5.

--- qh001/Pods/Target Support Files/Pods-qh001-qh001UITests/Pods-qh001-qh001UITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_qh001_qh001UITests : NSObject
-@end
-@implementation PodsDummy_Pods_qh001_qh001UITests
-@end
--- qh001/Pods/Target Support Files/Pods-qh001Tests/Pods-qh001Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_qh001Tests : NSObject
-@end
-@implementation PodsDummy_Pods_qh001Tests
-@end
